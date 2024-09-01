186f292f4f51ec177e12408a3ba6f3d1a7917bdb
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-09-01 20:28:42 +0300

    api folder __init__

--- core/home/views.py
+++ core/home/views.py
@@ -2,3 +2,20 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 
+@api_view(['GET'])
+def index(request):
+    courses = [
+        {
+            'id': 1,
+            'title': 'Course 1',
+            'description': 'Description 1',
+            'instructor': 'Instructor 1'
+        },
+        {
+            'id': 2,
+            'title': 'Course 2',
+            'description': 'Description 2',
+            'instructor': 'Instructor 2'
+        }
+    ]
+    return Response(courses)
