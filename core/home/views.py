a7670f0129cfeb0b095ab005c6f35d20e9c378d8
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-09-01 20:50:04 +0300

    add put methods

--- core/home/views.py
+++ core/home/views.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def index(request):
        
     courses = [
