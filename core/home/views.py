7849b38c9e89f4657d822f7b33e1f866a314e8b6
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-09-02 12:10:59 +0300

    add users in DB

--- core/home/views.py
+++ core/home/views.py
@@ -1,11 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
 from .models import Person
 from .serializers import PeopleSerializer
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def index(request):
-       
     courses = [
         {
             'id': 1,
@@ -21,7 +21,6 @@ def index(request):
         }
     ]
     
-    
     if request.method == 'GET':
         print(request.GET.get('search'))
         return Response(courses)
@@ -29,18 +28,18 @@ def index(request):
         data = request.data
         print(data)
         return Response({'message': 'Data created'}, status=201)
-    
     elif request.method == 'PUT':
         return Response({'message': 'Data updated'}, status=200)
     
     return Response(courses)
 
-api_view(['GET', 'POST', 'PUT', 'DELETE'])
+@csrf_exempt
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def people(request):
     if request.method == 'GET':
-        obj = Person.object.all()
-        Person = PeopleSerializer(obj, many=True)
-        return Response(Person.data)
+        obj = Person.objects.all()  # Corrected the query
+        serialized_data = PeopleSerializer(obj, many=True)  # Renamed the variable
+        return Response(serialized_data.data)
     
     if request.method == 'POST':
         data = request.data
@@ -49,6 +48,3 @@ def people(request):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
-     
-        
-        
